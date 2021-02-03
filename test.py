a21143c0285a9f36832bd458a751c7fdce3c1194
EvgeniyyEvgenievich <ku99826@gmail.com>
2021-02-03 18:53:14 +0300

    New begins

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-import requests
-import json
-import re
+class TestJ:
+    def __init__(self, number, count):
+        self.num = number
+        self.count = count
+        self.health = 100
+        self.speed = 1
 
-class TestJ():
-	def __init__(self,number,count):
-		self.num=num
-		self.count=count
+    def heals(self):
+        self.health += 10
+
+    def hits(self):
+        self.health -= 10
+
+    def buff(self):
+        self.speed += 10
+
+    def end_buff(self):
+        self.speed = 1
